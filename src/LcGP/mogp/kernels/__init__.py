391cc29646e2e863e3fd7bdd4333e2c17a97a78d
nasrmht <nassouradinemhamdan@gmail.com>
2026-03-24 21:44:18 +0100

    maj init

--- src/LcGP/mogp/kernels/__init__.py
+++ src/LcGP/mogp/kernels/__init__.py
@@ -1,3 +1,4 @@
 from .Kernel import Kernel, RBFKernel, Matern52Kernel
 from .LCMKernel import LMCKernel
+from .LMCKernelFree import LMCKernelFree
 from .ConstrainedLMCKernel import LMCKernelConstrained
